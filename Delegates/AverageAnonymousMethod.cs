1675f44cc09a03e24210b24a2eea4b6d0f651d1b
baditsa <vb94@ukr.net>
2016-03-29 21:06:26 +0300

    task 1,2

---
+++ Delegates/AverageAnonymousMethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{   
+    partial class Program
+    {
+        static AverageDelegate CodeBlock()
+        {
+            // вызов анонимного метода
+            AverageDelegate del = delegate(int arg1, int arg2, int arg3)  // этот м-д должен соответсвовать сигнатуре делегата
+            {
+                double result = 0;
+                result = (arg1 + arg2 + arg3) / 3d;
+                return result;
+            };
+            return del;
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    delegate double AverageDelegate(int arg1, int arg2, int arg3);
+    delegate int LengthLogin(string s);
+    delegate bool BoolPassword(string s1, string s2);
+    delegate double Operations(double a, double b);
+
+    partial class Program
+    {                
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter task:");
+            int task;
+            int.TryParse(Console.ReadLine(), out task);
+            switch (task)
+            {
+                case 1:
+                    AverageDelegate average = CodeBlock();
+                    Console.WriteLine(average(10,12,14));
+                    Console.WriteLine(average(1, 15, 15));
+                    break;
+                case 2:                                        
+                    double a = 10, b=12;                    
+
+                    Operations Add = (v1, v2) => v1 + v2;
+                    Console.WriteLine( Add(a,b));
+
+                    Add = (v1, v2) => v1 - v2;
+                    Console.WriteLine(Add(a, b));
+
+                    Add = (v1, v2) => v1 * v2;
+                    Console.WriteLine(Add(a, b));
+
+                    Add = (v1, v2) =>
+                        {
+                            if (v2 != 0)
+                                return v1 / v2;
+                            else
+                            {
+                                Console.WriteLine("Error: division by zero. Exit code: -1");
+                                return -1;
+                            }
+                        };
+                    Console.WriteLine(Add(a, b));
+                    
+                    break;
+                case 3:
+                    break;
+            }            
+            Console.ReadKey();
+        }
+    }
+}
